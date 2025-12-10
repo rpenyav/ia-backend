84e471704fefb0151eac5ebd613864b016bbba09
Rafa <rafa@wad.cat>
2025-12-10 19:21:21 +0100

    feat: added products

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
 // src/users/dto/create-user.dto.ts
+// src/users/dto/create-user.dto.ts
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
+
+  @IsOptional()
+  @IsString()
   name?: string;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,5 +1,17 @@
 // src/users/dto/update-user.dto.ts
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateUserDto } from "./create-user.dto";
+import { IsEmail, IsOptional, IsString, MinLength } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+}
